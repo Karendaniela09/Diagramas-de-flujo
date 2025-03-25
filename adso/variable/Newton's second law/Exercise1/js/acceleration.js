ef18c4defd2e7e6326f8cd7d3059f47409f36658
Karendaniela09 <klddaniela8@gmail.com>
2025-03-24 22:10:21 -0500

    Add files via upload

---
+++ adso/variable/Newton's second law/Exercise1/js/acceleration.js
@@ -0,0 +1,13 @@
+﻿/*
+	Name exercise: Newton's second law
+	Description: Newton's second law
+	Autor: Karen Daniela Holguín Cruz
+	Date: march 19th 2025
+*/
+
+let appliedForce=12;
+let mass=20;
+let acceleration;
+
+acceleration=appliedForce/mass
+console.log("Acceleration: "+acceleration);	
